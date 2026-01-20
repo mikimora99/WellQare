81918af8f2366555d8555f746583eb190364ee77
mikimora99 <mdemora999@gmail.com>
2026-01-20 15:23:01 +0100

    Add Firebase configuration file

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfPp0py-xUrWLyS07g3UoixE7k5Q5YssM",
+  authDomain: "wellcare-1796c.firebaseapp.com",
+  projectId: "wellcare-1796c",
+  storageBucket: "wellcare-1796c.firebasestorage.app",
+  messagingSenderId: "809832042517",
+  appId: "1:809832042517:web:13754f96fde66ed3357e1c",
+  measurementId: "G-Q1C1V4NYXZ",
+} as const;
+
+export default firebaseConfig;
